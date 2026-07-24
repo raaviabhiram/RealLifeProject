0fcee848ea54cc3abd3feb1f9787fca80d2a1822
Abhiram <abhiram.raavi123@gmail.com>
2026-07-24 11:54:03 +0530

    first commit

---
+++ src/main/java/com/Project/RealLifeProject/RealLifeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Project.RealLifeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealLifeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealLifeProjectApplication.class, args);
+	}
+
+}
